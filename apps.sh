d6b27c7755db8c0820ec8d45ccf613a6ea40d839
Oliver Buchmann <oliver@Olivers-MacBook-Pro.local>
2015-11-08 14:21:35 +0100

    El Capitan

--- apps.sh
+++ apps.sh
@@ -1,13 +1,13 @@
 brew install caskroom/cask/brew-cask
 
 # Dependencys 
-brew cask install caskroom/versions/java7 ## for intellij-idea
+brew cask install caskroom/versions/java8 ## for intellij-idea
 
 # Terminal
 brew cask install iterm2
 
 # Commandline Apps
-brew install fig
+#brew install fig
 
 # Browsers
 brew cask install google-chrome
@@ -24,15 +24,21 @@ brew cask install intellij-idea
 brew cask install atom
 brew cask install phpstorm
 brew cask install brackets
+brew cask install android-studio
+brew cask install visual-studio-code
+
+# Git
+brew cask install sourcetree
+
 
 # Dev Env
 ## Virtualisation
-brew cask install virtualbox
+#brew cask install virtualbox
 ### Docker
-brew install boot2docker # installs docker as dependency
+#brew install boot2docker # installs docker as dependency
 ### Vagrant
-brew cask install vagrant
-brew cask install vagrant-manager
+#brew cask install vagrant
+#brew cask install vagrant-manager
 
 # Communication
 brew cask install skype
@@ -48,7 +54,11 @@ brew cask install sizeup
 brew cask install tunnelblick
 
 # Security
-brew cask install lastpass
+#brew cask install lastpass
+brew install pass
 
 ## open lastpass installer
-open /opt/homebrew-cask/Caskroom/lastpass/latest/LastPass Installer.app
+#open /opt/homebrew-cask/Caskroom/lastpass/latest/LastPass Installer.app
+
+# Entertainment 
+brew cask install spotify
